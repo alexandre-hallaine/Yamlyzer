c38a7176c736b6e12585a3a669a4fa77f12b044b
Alexandre Hallaine <alexandre@hallaine.com>
2025-06-09 08:12:44 -0400

    feat: initial project setup with NuxtHub, UI Pro, ESLint, and Drizzle (#1)
    * feat: README
    
    * feat(package): update and cleanup
    
    * feat: nuxt ui pro
    
    * feat: nuxt eslint
    
    * ci: add nuxthub workflow
    
    * feat: nuxt hub
    
    * feat(edge): refactor config
    
    * feat(edge): db
    
    ---------
    
    Co-authored-by: nuxthub-admin[bot] <163076504+nuxthub-admin[bot]@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/ui-pro', '@nuxt/eslint', '@nuxthub/core'],
+  $development: { hub: { remote: 'production' } },
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  nitro: { experimental: { openAPI: true } },
+  hub: {
+    workers: true,
+    database: true,
+    bindings: { compatibilityDate: '2025-05-05' },
+  },
+  eslint: {
+    config: { stylistic: true },
+    checker: true,
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <UPageHero title="Hey" />
+</template>

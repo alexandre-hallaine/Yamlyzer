10bffc56c8882929b12e918e6aa24e4ea82c303c
Alexandre Hallaine <alexandre@hallaine.com>
2025-06-09 11:38:31 -0400

    feat: composable

---
+++ composables/stream.ts
@@ -0,0 +1,42 @@
+export function useStream(endpoint: string, body: string) {
+  async function* response() {
+    const response = await $fetch(endpoint, {
+      method: 'POST',
+      body,
+      responseType: 'stream',
+      headers: { 'Content-Type': 'text/plain' },
+    })
+    if (!(response instanceof ReadableStream)) throw new Error('Expected a stream response')
+
+    let buffer = ''
+    const reader = (response as ReadableStream).pipeThrough(new TextDecoderStream()).getReader()
+
+    while (true) {
+      const { value, done } = await reader.read()
+
+      if (done) {
+        if (buffer.trim()) console.warn('Stream ended with unparsed data:', buffer)
+        return
+      }
+
+      buffer += value
+      const lines = buffer.split('\n')
+      buffer = lines.pop() || ''
+
+      for (const line of lines) {
+        if (!line.startsWith('data: ')) continue
+        const data = line.slice('data: '.length).trim()
+
+        try {
+          const jsonData = JSON.parse(data)
+          if (jsonData.response) yield jsonData.response
+        }
+        catch (parseError) {
+          console.warn('Error parsing JSON:', parseError)
+        }
+      }
+    }
+  }
+
+  return response
+}
--- pages/index.vue
+++ pages/index.vue
@@ -4,46 +4,11 @@ const output = ref('')
 const loading = ref(false)
 
 async function convert() {
-  output.value = ''
   loading.value = true
-  console.log('convert')
+  output.value = ''
 
-  const response = await $fetch<ReadableStream>('/api/convert', {
-    method: 'POST',
-    body: input.value,
-    responseType: 'stream',
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-  })
-
-  let buffer = ''
-  const reader = response.pipeThrough(new TextDecoderStream()).getReader()
-
-  while (true) {
-    const { value, done } = await reader.read()
-    if (done) {
-      if (buffer.trim()) console.warn('Stream ended with unparsed data:', buffer)
-      break
-    }
-
-    buffer += value
-    const lines = buffer.split('\n')
-    buffer = lines.pop() || ''
-
-    for (const line of lines) {
-      if (!line.startsWith('data: ')) continue
-      const data = line.slice('data: '.length).trim()
-
-      try {
-        const jsonData = JSON.parse(data)
-        if (jsonData.response) output.value += jsonData.response
-      }
-      catch (parseError) {
-        console.warn('Error parsing JSON:', parseError)
-      }
-    }
-  }
+  const response = useStream('/api/convert', input.value)()
+  for await (const chunk of response) output.value += chunk
 
   loading.value = false
 }

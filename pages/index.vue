759ad5b7f31a5382afa5780bf35fb3a16a5c6828
Alexandre Hallaine <alexandre@hallaine.com>
2025-06-09 12:11:11 -0400

    feat: Implement `useStream` composable and enhance `index` page design (#3)
    * feat: composable
    
    * feat: design

--- pages/index.vue
+++ pages/index.vue
@@ -4,60 +4,41 @@ const output = ref('')
 const loading = ref(false)
 
 async function convert() {
-  output.value = ''
   loading.value = true
-  console.log('convert')
+  output.value = ''
 
-  const response = await $fetch<ReadableStream>('/api/convert', {
-    method: 'POST',
-    body: input.value,
-    responseType: 'stream',
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-  })
-
-  let buffer = ''
-  const reader = response.pipeThrough(new TextDecoderStream()).getReader()
-
-  while (true) {
-    const { value, done } = await reader.read()
-    if (done) {
-      if (buffer.trim()) console.warn('Stream ended with unparsed data:', buffer)
-      break
-    }
-
-    buffer += value
-    const lines = buffer.split('\n')
-    buffer = lines.pop() || ''
-
-    for (const line of lines) {
-      if (!line.startsWith('data: ')) continue
-      const data = line.slice('data: '.length).trim()
-
-      try {
-        const jsonData = JSON.parse(data)
-        if (jsonData.response) output.value += jsonData.response
-      }
-      catch (parseError) {
-        console.warn('Error parsing JSON:', parseError)
-      }
-    }
-  }
+  const response = useStream('/api/convert', input.value)()
+  for await (const chunk of response) output.value += chunk
 
   loading.value = false
 }
 </script>
 
 <template>
-  <UPageSection>
-    <UPageHero title="Hey" />
-    <UTextarea v-model="input" />
-    <UButton
-      label="Convert"
-      :loading
-      @click="convert"
-    />
-    <pre>{{ output }}</pre>
+  <UPageSection
+    title="Yamlyzer"
+    description="Convert raw logs, configs, or CLI output into structured YAML instantly."
+  >
+    <template #links>
+      <UButton
+        label="Convert"
+        :loading
+        @click="convert"
+      />
+    </template>
+
+    <UPageGrid class="lg:grid-cols-2">
+      <UTextarea
+        v-model="input"
+        autoresize
+        placeholder="Paste raw text here..."
+        :ui="{ base: 'h-full' }"
+      />
+      <UCard>
+        <pre class="whitespace-pre-wrap">
+          {{ output }}
+        </pre>
+      </UCard>
+    </UPageGrid>
   </UPageSection>
 </template>

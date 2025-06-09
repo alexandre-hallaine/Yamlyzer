69b9e257791a74437e17f1294751a06ea52d780c
Alexandre Hallaine <alexandre@hallaine.com>
2025-06-09 08:03:40 -0400

    feat: nuxt hub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui-pro', '@nuxt/eslint'],
+  modules: ['@nuxt/ui-pro', '@nuxt/eslint', '@nuxthub/core'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-05-15',
+  hub: { worker: true },
   eslint: {
     config: { stylistic: true },
     checker: true,

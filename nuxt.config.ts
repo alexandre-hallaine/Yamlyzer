f0429d1e1baa33f9f29d06180e6fb335e2354e93
Alexandre Hallaine <alexandre@hallaine.com>
2025-06-09 08:06:57 -0400

    feat(edge): refactor config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/ui-pro', '@nuxt/eslint', '@nuxthub/core'],
+  $development: { hub: { remote: 'production' } },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-05-15',
-  hub: { worker: true },
+  nitro: { experimental: { openAPI: true } },
+  hub: {
+    workers: true,
+    bindings: { compatibilityDate: '2025-05-05' },
+  },
   eslint: {
     config: { stylistic: true },
     checker: true,
